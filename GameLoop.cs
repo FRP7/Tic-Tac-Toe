9c9651b72a7edc129555f6be500dbc0747b90715
FranciscoPires <frelvaspires@gmail.com>
2020-11-14 14:59:01 +0000

    verificar vitória (wip)

--- GameLoop.cs
+++ GameLoop.cs
@@ -7,6 +7,55 @@ namespace TicTacToe
     /// </summary>
     class GameLoop
     {
+        public bool CheckWin() {
+            Table table = new Table();
+            Console.WriteLine("A checkwin está a correr");
+            // Horizontais
+            if (table.slots[0] == "X" && table.slots[1] == "X" 
+                && table.slots[2] == "X") {
+                Console.WriteLine("Ganhou!!!");
+                return true;
+            } else if (table.slots[3] == "X" && table.slots[4] == "X"
+               && table.slots[5] == "X") {
+                Console.WriteLine("Ganhou!!!");
+                return true;
+            } else if (table.slots[6] == "X" && table.slots[7] == "X"
+                 && table.slots[8] == "X") {
+                Console.WriteLine("Ganhou!!!");
+                return true;
+            }
+              //
+              // Verticais
+              else if (table.slots[0] == "X" && table.slots[3] == "X"
+                  && table.slots[6] == "X") {
+                Console.WriteLine("Ganhou!!!");
+                return true;
+            } else if (table.slots[1] == "X" && table.slots[4] == "X"
+               && table.slots[7] == "X") {
+                Console.WriteLine("Ganhou!!!");
+                return true;
+            } else if (table.slots[2] == "X" && table.slots[5] == "X"
+                 && table.slots[8] == "X") {
+                Console.WriteLine("Ganhou!!!");
+                return true;
+            }
+            //
+            // Diagonais
+            else if (table.slots[0] == "X" && table.slots[4] == "X"
+                && table.slots[8] == "X") {
+                Console.WriteLine("Ganhou!!!");
+                return true;
+            } else if (table.slots[2] == "X" && table.slots[4] == "X"
+               && table.slots[6] == "X") {
+                Console.WriteLine("Ganhou!!!");
+                return true;
+            }
+            //
+            // Se não há linha em nenhum desses lados, retorna falso.
+            else {
+                return false;
+            }
+        }
     }
     //
 }
--- Table.cs
+++ Table.cs
@@ -9,7 +9,7 @@ namespace TicTacToe
     /// </summary>
     class Table
     {
-        private string[] slots = new string[] {
+        public string[] slots = new string[] {
             "1", "2", "3", "4", "5", "6", "7", "8", "9"
         };
 
@@ -26,40 +26,42 @@ namespace TicTacToe
         }
 
         public void Mark() {
+            GameLoop gameloop = new GameLoop();
             string userinput;
-            while(true) {
-                Console.Clear();
-                Console.WriteLine("Escolhe uma slot para colocar X");
-                DrawTable();
-                userinput = Console.ReadLine();
-                if(userinput == slots[0]) {
-                    slots[0] = "X";
-                }
-                if (userinput == slots[1]) {
-                    slots[1] = "X";
-                }
-                if (userinput == slots[2]) {
-                    slots[2] = "X";
-                }
-                if (userinput == slots[3]) {
-                    slots[3] = "X";
-                }
-                if (userinput == slots[4]) {
-                    slots[4] = "X";
-                }
-                if (userinput == slots[5]) {
-                    slots[5] = "X";
-                }
-                if (userinput == slots[6]) {
-                    slots[6] = "X";
-                }
-                if (userinput == slots[7]) {
-                    slots[7] = "X";
-                }
-                if (userinput == slots[8]) {
-                    slots[8] = "X";
+            while (gameloop.CheckWin() == false) {
+                    Console.Clear();
+                    Console.WriteLine("Escolhe uma slot para colocar X");
+                    DrawTable();
+                    userinput = Console.ReadLine();
+                    if (userinput == slots[0]) {
+                        slots[0] = "X";
+                    }
+                    if (userinput == slots[1]) {
+                        slots[1] = "X";
+                    }
+                    if (userinput == slots[2]) {
+                        slots[2] = "X";
+                    }
+                    if (userinput == slots[3]) {
+                        slots[3] = "X";
+                    }
+                    if (userinput == slots[4]) {
+                        slots[4] = "X";
+                    }
+                    if (userinput == slots[5]) {
+                        slots[5] = "X";
+                    }
+                    if (userinput == slots[6]) {
+                        slots[6] = "X";
+                    }
+                    if (userinput == slots[7]) {
+                        slots[7] = "X";
+                    }
+                    if (userinput == slots[8]) {
+                        slots[8] = "X";
+                    }
+                    gameloop.CheckWin();
                 }
             }
         }
     }
-}
