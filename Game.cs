75de9c0ca85691f0a9719edfc06a088f03c1574d
FranciscoPires <frelvaspires@gmail.com>
2020-11-18 22:24:41 +0000

    win condition básica feita

--- Game.cs
+++ Game.cs
@@ -4,18 +4,17 @@ namespace TicTacToe
 {
     class Game
     {
-        Player player = new Player();
-
         public static string[] slots = new string[]
            {
                 "1", "2", "3", "4", "5", "6", "7", "8", "9"
            };
 
         public void GameLoop() {
+            Player player = new Player();
             Table table = new Table();
             AI ai = new AI();
 
-            while(CheckWin() == false) {
+            while (CheckWin() == false) {
                 Console.WriteLine("Game loop");
                 table.DrawTable();
                 player.Play();
@@ -26,8 +25,102 @@ namespace TicTacToe
         }
 
         public bool CheckWin() {
-            Console.WriteLine("Check win");
+            int count = 0;
+
+            // Horizontais
+            for (int i = 0; i < 3; i++) {
+                if (slots[i] == "X") {
+                    count++;
+                }
+                if (count == 3) {
+                    Console.WriteLine("Ganhou!!!");
+                    return true;
+                }
+            }
+
+            count = default;
+            for (int i = 3; i < 6; i++) {
+                if (slots[i] == "X") {
+                    count++;
+                }
+                if (count == 3) {
+                    Console.WriteLine("Ganhou!!!");
+                    return true;
+                }
+            }
+
+            count = default;
+            for (int i = 6; i < 9; i++) {
+                if (slots[i] == "X") {
+                    count++;
+                }
+                if (count == 3) {
+                    Console.WriteLine("Ganhou!!!");
+                    return true;
+                }
+            }
+            //
+            // Verticais
+            count = default;
+            for (int i = 0; i < 7; i += 3) {
+                if (slots[i] == "X") {
+                    count++;
+                }
+                if (count == 3) {
+                    Console.WriteLine("Ganhou!!!");
+                    return true;
+                }
+            }
+
+            count = default;
+            for (int i = 1; i < 8; i += 3) {
+                if (slots[i] == "X") {
+                    count++;
+                }
+                if (count == 3) {
+                    Console.WriteLine("Ganhou!!!");
+                    return true;
+                }
+            }
+
+            count = default;
+            for (int i = 2; i < 9; i += 3) {
+                if (slots[i] == "X") {
+                    count++;
+                }
+                if (count == 3) {
+                    Console.WriteLine("Ganhou!!!");
+                    return true;
+                }
+            }
+
+            //
+            // Diagonais
+            count = default;
+            for (int i = 0; i < 9; i += 4) {
+                if (slots[i] == "X") {
+                    count++;
+                }
+                if (count == 3) {
+                    Console.WriteLine("Ganhou!!!");
+                    return true;
+                }
+            }
+
+            count = default;
+            for (int i = 2; i < 7; i += 2) {
+                if (slots[i] == "X") {
+                    count++;
+                }
+                if (count == 3) {
+                    Console.WriteLine("Ganhou!!!");
+                    return true;
+                }
+            }
+            //
+            // Se não há linha em nenhum desses lados, retorna falso.
             return false;
+            //
         }
     }
 }
--- Table.cs
+++ Table.cs
@@ -5,7 +5,6 @@ namespace TicTacToe
     class Table
     {
         public void DrawTable() {
-            Game game = new Game();
             Console.WriteLine($"     |     |   ");
             Console.WriteLine($"  {Game.slots[0]}  |  {Game.slots[1]}  |  {Game.slots[2]}   ");
             Console.WriteLine($"_____|_____|_____");
@@ -15,10 +14,6 @@ namespace TicTacToe
             Console.WriteLine($"     |     |   ");
             Console.WriteLine($"  {Game.slots[6]}  |  {Game.slots[7]}  |  {Game.slots[8]}  ");
             Console.WriteLine($"     |     |   ");
-
-            foreach(var teste in Game.slots) {
-                Console.WriteLine("Slots: " + teste);
-            }
         }
     }
 }
