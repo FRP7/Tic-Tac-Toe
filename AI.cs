52c052889bb1102aaff77a18c0d6bae64ec49b52
FranciscoPires <frelvaspires@gmail.com>
2020-11-19 21:34:57 +0000

    minimax (wip)

--- AI.cs
+++ AI.cs
@@ -7,11 +7,6 @@ namespace TicTacToe
     /// Classe onde o computador joga.
     /// </summary>
     class AI {
-        // Avaliação estática.
-        private static int heuristic;
-        private int maxEval;
-        private int minEval;
-        private string eval;
 
         // Dicionário de jogadas possíveis com o score a 0 por default
         private static Dictionary<int, int> possiblePlays = new Dictionary<int, int>() {
@@ -29,56 +24,67 @@ namespace TicTacToe
         // Método onde a AI joga
         public void Play() {
             Console.WriteLine("Turno da AI");
-            //Minimax();
-            /*Console.WriteLine("Turno da AI");
-            Random rnd = new Random();
-            int rndPlay = rnd.Next(1, 10);
-            bool isPlayed = false;
-
-            while(isPlayed == false) {
-                if(Game.slots[rndPlay -1] != "X" && Game.slots[rndPlay -1] != "O") {
-                    Game.slots[rndPlay - 1] = "O";
-                    isPlayed = true;
-                }
-                else {
-                    rndPlay = rnd.Next(1, 10);
-                }
-            }*/
+            Minimax(2000, 0);
+            Game.slots[HighestScore()] = "O";
         }
 
         private void Evaluate() {
             if(Game.slots[0] == "X") {
-                
+                possiblePlays[1] += 1;
+            }
+            if (Game.slots[1] == "X") {
+                possiblePlays[4] += 1;
+            }
+            if (Game.slots[2] == "X") {
+                possiblePlays[1] += 1;
+            }
+            if (Game.slots[3] == "X") {
+                possiblePlays[4] += 1;
+            }
+            if (Game.slots[4] == "X") {
+                possiblePlays[1] += 1;
+            }
+            if (Game.slots[5] == "X") {
+                possiblePlays[4] += 1;
+            }
+            if (Game.slots[6] == "X") {
+                possiblePlays[7] += 1;
+            }
+            if (Game.slots[7] == "X") {
+                possiblePlays[4] += 1;
+            }
+            if (Game.slots[8] == "X") {
+                possiblePlays[7] += 1;
             }
         }
 
-        private void Minimax(string position, int depth, bool maximizingPlayer) {
-            /*if(depth == 0 ou que o jogo está perdido na posição Y) { 
-                // retomar avaliação de position
-            }*/
-
-            if (maximizingPlayer == true) {
-                maxEval = -10000;
-                // foreach de lista de posições possívels
-                /*foreach(var item in lista)
-                 * {
-                 * eval = minimax(item, depth -1, false);
-                 * maxEval = max(maxEval, eval) buscar o valor mais alto
-                 * }
-                 * retomar maxEval
-                 */
-            } else {
-                minEval = 10000;
-                /*
-                 * foreach de lista de posições possíveis
-                 * foreach(var item in lista)
-                 * {
-                 * eval = minimax(item, depth -1, true);
-                 * minEval = min(minEval, eval)
-                 * }
-                 * retomar minEval
-                 */
+        private void Minimax(int maxDepth, int currentDepth) {
+            if(currentDepth < maxDepth) {
+                /* Para cada jogada possível, chamar Minimax(currentDepth +1)
+                 * Devolver melhor jogada*/
+                foreach(KeyValuePair<int, int> play in possiblePlays) {
+                    Minimax(maxDepth, currentDepth + 1);
+                    Evaluate();
+                }
             }
+        }
+
+        private int HighestScore() {
+            // Jogada escolhida
+            int chosenPlay;
+            // Valor máximo da jogada
+            int maxValue;
+            // Converter os valores do dicionário para array
+            int[] highest = new int[possiblePlays.Count];
+            possiblePlays.Values.CopyTo(highest, 0);
+            // Ordenar a array por ordem crescente
+            Array.Sort(highest);
+            // Buscar o valor que está no topo (que é o mais alto)
+            maxValue = highest[highest.Length - 1];
+            // Procurar chave do valor máximo no dicionário
+            chosenPlay = possiblePlays[maxValue];
+            // Devolver a jogada escolhida
+            return chosenPlay;
         }
     }
 }
