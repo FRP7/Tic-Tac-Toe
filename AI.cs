a0636c9b9a417fed0b0fa70a35a741df6d4ad17a
FranciscoPires <frelvaspires@gmail.com>
2020-11-19 18:27:39 +0000

    AI feita (random)

--- AI.cs
+++ AI.cs
@@ -10,6 +10,19 @@ namespace TicTacToe
         // Método onde a AI joga
         public void Play() {
             Console.WriteLine("Turno da AI");
+            Random rnd = new Random();
+            int rndPlay = rnd.Next(1, 10);
+            bool isPlayed = false;
+
+            while(isPlayed == false) {
+                if(Game.slots[rndPlay -1] != "X" && Game.slots[rndPlay -1] != "O") {
+                    Game.slots[rndPlay - 1] = "O";
+                    isPlayed = true;
+                }
+                else {
+                    rndPlay = rnd.Next(1, 10);
+                }
+            }
         }
     }
 }
--- Game.cs
+++ Game.cs
@@ -32,96 +32,201 @@ namespace TicTacToe
         // Método que verifica se existe vitória.
         public bool CheckWin() {
             int count = 0;
+            int countAi = 0;
 
-            // Horizontais
+            // Horizontais 
+            // Jogador
             for (int i = 0; i < 3; i++) {
                 if (slots[i] == "X") {
                     count++;
                 }
                 if (count == 3) {
-                    Console.WriteLine("Ganhou!!!");
+                    Console.WriteLine("Ganhou o jogador!!!");
                     return true;
                 }
             }
-
             count = default;
+            // AI
+            for (int i = 0; i < 3; i++) {
+                if (slots[i] == "O") {
+                    countAi++;
+                }
+                if (countAi == 3) {
+                    Console.WriteLine("Ganhou a Ai!!!");
+                    return true;
+                }
+            }
+            countAi = default;
+
+            // Jogador
             for (int i = 3; i < 6; i++) {
                 if (slots[i] == "X") {
                     count++;
                 }
                 if (count == 3) {
-                    Console.WriteLine("Ganhou!!!");
+                    Console.WriteLine("Ganhou o jogador!!!");
                     return true;
                 }
             }
-
             count = default;
+
+            // Ai
+            for (int i = 3; i < 6; i++) {
+                if (slots[i] == "O") {
+                    countAi++;
+                }
+                if (countAi == 3) {
+                    Console.WriteLine("Ganhou a AI!!!");
+                    return true;
+                }
+            }
+            countAi = default;
+
+            // Jogador
             for (int i = 6; i < 9; i++) {
                 if (slots[i] == "X") {
                     count++;
                 }
                 if (count == 3) {
-                    Console.WriteLine("Ganhou!!!");
+                    Console.WriteLine("Ganhou o jogador!!!");
                     return true;
                 }
             }
+            count = default;
+
+            // Ai
+            for (int i = 6; i < 9; i++) {
+                if (slots[i] == "O") {
+                    countAi++;
+                }
+                if (countAi == 3) {
+                    Console.WriteLine("Ganhou a AI!!!");
+                    return true;
+                }
+            }
+            countAi = default;
 
             // Verticais
-            count = default;
+            // Jogador
             for (int i = 0; i < 7; i += 3) {
                 if (slots[i] == "X") {
                     count++;
                 }
                 if (count == 3) {
-                    Console.WriteLine("Ganhou!!!");
+                    Console.WriteLine("Ganhou o jogador!!!");
                     return true;
                 }
             }
-
             count = default;
+
+            // Ai
+            for (int i = 0; i < 7; i += 3) {
+                if (slots[i] == "O") {
+                    countAi++;
+                }
+                if (countAi == 3) {
+                    Console.WriteLine("Ganhou a AI!!!");
+                    return true;
+                }
+            }
+            countAi = default;
+
+            // Jogador
             for (int i = 1; i < 8; i += 3) {
                 if (slots[i] == "X") {
                     count++;
                 }
                 if (count == 3) {
-                    Console.WriteLine("Ganhou!!!");
+                    Console.WriteLine("Ganhou o jogador!!!");
                     return true;
                 }
             }
-
             count = default;
+
+            // Ai
+            for (int i = 1; i < 8; i += 3) {
+                if (slots[i] == "O") {
+                    countAi++;
+                }
+                if (countAi == 3) {
+                    Console.WriteLine("Ganhou a AI!!!");
+                    return true;
+                }
+            }
+            countAi = default;
+
+            // Jogador
             for (int i = 2; i < 9; i += 3) {
                 if (slots[i] == "X") {
                     count++;
                 }
                 if (count == 3) {
-                    Console.WriteLine("Ganhou!!!");
+                    Console.WriteLine("Ganhou o jogador!!!");
                     return true;
                 }
             }
+            count = default;
+
+            // Ai
+            for (int i = 2; i < 9; i += 3) {
+                if (slots[i] == "O") {
+                    countAi++;
+                }
+                if (countAi == 3) {
+                    Console.WriteLine("Ganhou a AI!!!");
+                    return true;
+                }
+            }
+            countAi = default;
 
             // Diagonais
-            count = default;
+            // Jogador
             for (int i = 0; i < 9; i += 4) {
                 if (slots[i] == "X") {
                     count++;
                 }
                 if (count == 3) {
-                    Console.WriteLine("Ganhou!!!");
+                    Console.WriteLine("Ganhou o jogador!!!");
                     return true;
                 }
             }
-
             count = default;
+
+            // Ai
+            for (int i = 0; i < 9; i += 4) {
+                if (slots[i] == "O") {
+                    countAi++;
+                }
+                if (countAi == 3) {
+                    Console.WriteLine("Ganhou a AI!!!");
+                    return true;
+                }
+            }
+            countAi = default;
+
+            // Jogador
             for (int i = 2; i < 7; i += 2) {
                 if (slots[i] == "X") {
                     count++;
                 }
                 if (count == 3) {
-                    Console.WriteLine("Ganhou!!!");
+                    Console.WriteLine("Ganhou o jogador!!!");
                     return true;
                 }
             }
+            count = default;
+
+            // Ai
+            for (int i = 2; i < 7; i += 2) {
+                if (slots[i] == "O") {
+                    countAi++;
+                }
+                if (countAi == 3) {
+                    Console.WriteLine("Ganhou a AI!!!");
+                    return true;
+                }
+            }
+            countAi = default;
 
             // Se não há linha em nenhum desses lados, retorna falso.
             return false;
